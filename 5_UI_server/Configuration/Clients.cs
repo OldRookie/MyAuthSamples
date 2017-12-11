5a1cef564afdec6ae7039fef2f7cc64ad15c1b59
Sergio Navarro Pino <sergio.navarro@analyticalways.com>
2017-12-11 16:55:52 +0100

    Authorization code grant server

---
+++ 5_UI_server/Configuration/Clients.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace _5_UI_server.Configuration
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "codeClient",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" },
+                    RequireConsent = true,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string> { "http://localhost:3000/callback" },
+                    PostLogoutRedirectUris = new List<string> {"http://localhost:3000/logout"}
+                }
+            };
+        }
+    }
+}
---
+++ 5_UI_server/Configuration/Users.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Test;
+
+namespace _5_UI_server.Configuration
+{
+    public class Users
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "efrain",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "tiberio",
+                    Password = "password"
+                }
+            };
+        }
+}
+}
